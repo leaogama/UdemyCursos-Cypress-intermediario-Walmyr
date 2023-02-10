4a40af303e042a58520309fe37602bcfe2b38425
Flávio Leão da Gama <38006748+leaogama@users.noreply.github.com>
2023-02-10 17:39:49 -0300

    CreateProject ok

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -11,9 +11,19 @@ Cypress.Commands.add('login', (
     login()
 })
 
-Cypress.Commands.add('logout', () =>{
+Cypress.Commands.add('logout', () => {
     cy.get('.qa-user-avatar').should('be.visible')
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
     cy.get('[data-qa-selector="sign_in_tab"]').should('be.visible')
+})
+
+
+Cypress.Commands.add('gui_createProject', (product, description) => {
+    cy.visit('/projects/new')
+    cy.get('#blank-project-name > .project-name > #project_name').type(product)
+    cy.get('#project_description').type(description)
+    cy.get('[data-qa-selector="private_radio"]').should('be.visible').first().check()
+    cy.get('#project_initialize_with_readme').should('be.visible').check()
+    cy.get('#blank-project-pane > #new_project > .btn-success').click()
 })
